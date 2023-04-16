679fa27fd1be38f6fd2c76765343e7de467812df
Dmitriy Bogdanov <dmitry.b@digital-spectr.ru>
2023-04-16 23:52:13 +0300

    Добавил тему

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "iceberg",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
